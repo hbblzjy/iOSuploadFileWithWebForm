a500bf01bede744673923ce735cdd8788176f49e
hbblzjy <hbblzjy@163.com>
2017-05-05 16:10:46 +0800

    Initial Commit

---
+++ UploadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UploadDemo
+//
+//  Created by JasonHao on 2017/5/5.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UploadDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UploadDemo
+//
+//  Created by JasonHao on 2017/5/5.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
